3add23e66488436c5fa070c3b9e786db3deb3d26
Caolan Maguire <caolanmichaelmaguire@gmail.com>
2025-12-02 13:47:18 +0000

    updated .gitignore

--- scripts/01_data_preprocessing.R
+++ scripts/01_data_preprocessing.R
@@ -10,3 +10,17 @@ library(tidyverse)
 library(DESeq2)
 
 cat("Env setup\n")
+
+# - - - - - - - - - - - - - - - 
+# Load Data Files
+# - - - - - - - - - - - - - - - 
+
+cat(getwd())
+
+cat("- - - Loading Files - - - \n")
+rna_data <- read.delim("data/data_mrna_seq_v2_rsem.txt",
+                       header = TRUE,
+                       row.names = 1,
+                       check.names = FALSE)
+
+cat("RNA data loaded \n")
