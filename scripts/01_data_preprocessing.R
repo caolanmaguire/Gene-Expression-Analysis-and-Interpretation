2fb24e7027f3dce5c4c9ce4c1101c755013ca967
Caolan Maguire <caolanmichaelmaguire@gmail.com>
2025-12-02 15:45:53 +0000

    changed feature on reading in relevant datasets - now added a for l

--- scripts/01_data_preprocessing.R
+++ scripts/01_data_preprocessing.R
@@ -3,7 +3,16 @@
 # Assignment: Assignment 2
 
 # set working dir
-setwd("C:/Users/caola/OneDrive/Documents/GitHub/Gene-Expression-Analysis-and-Interpretation/")
+
+if (require("rstudioapi", quietly = TRUE) && rstudioapi::isAvailable()) {
+  project_dir <- rstudioapi::selectDirectory(
+    caption = "Select Project Root Directory",
+    label = "Select"
+  )
+}else{
+  setwd("C:/Users/caola/OneDrive/Documents/GitHub/Gene-Expression-Analysis-and-Interpretation/")
+}
+
 
 # Load libraries
 library(tidyverse)
@@ -15,19 +24,59 @@ cat("Env setup\n")
 # Load Data Files
 # - - - - - - - - - - - - - - - 
 
-cat(getwd())
-cat("\n")
+# cat(getwd())
+# cat("\n")
 
-cat("- - - Loading Files - - - \n")
-rna_data <- read.delim("data/data_mrna_seq_v2_rsem.txt",
-                       header = TRUE,
-                       check.names = FALSE)
+# Define files to load
+data_files <- list(
+  rna = "data/data_mrna_seq_v2_rsem.txt",
+  clinical = "data/data_clinical_patient.txt",
+  cna = "data/data_cna.txt"
+)
 
-cat("RNA data loaded \n")
+# Create empty list to store data
+datasets <- list()
 
-cat("✓ RNA data loaded (raw)\n")
-cat("Dimensions:", nrow(rna_data), "rows x", ncol(rna_data), "columns\n")
+# Loop through and load each file
+for (name in names(data_files)) {
+  file_path <- data_files[[name]]
+  
+  cat("Loading", name, "data from:", basename(file_path), "\n")
+  
+  if (file.exists(file_path)) {
+    
+    # Special handling for clinical data (has comment lines)
+    if (name == "clinical") {
+      datasets[[name]] <- read.delim(
+        file_path,
+        header = TRUE,
+        comment.char = "#",
+        na.strings = c("", "NA", "[Not Available]", "[Not Applicable]"),
+        check.names = FALSE
+      )
+    } else {
+      # RNA and CNA data
+      datasets[[name]] <- read.delim(
+        file_path,
+        header = TRUE,
+        check.names = FALSE
+      )
+    }
+    
+    cat("  ✓ Loaded:", nrow(datasets[[name]]), "rows x", 
+        ncol(datasets[[name]]), "columns\n")
+    
+    cat("\nFirst few column names:\n")
+    print(head(colnames(datasets[[]])))
+    
+  } else {
+    stop("ERROR: File not found - ", file_path)
+  }
+}
+
+# cat("✓ RNA data loaded (raw)\n")
+# cat("Dimensions:", nrow(rna_data), "rows x", ncol(rna_data), "columns\n")
 
 # Check first few columns
-cat("\nFirst few column names:\n")
-print(head(colnames(rna_data)))
+# cat("\nFirst few column names:\n")
+# print(head(colnames(rna_data)))
